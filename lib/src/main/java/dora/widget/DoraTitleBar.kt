7f1d94f5820983af9e6c36d79585640bf89d2047
dora4 <71242257+dora4@users.noreply.github.com>
2023-10-09 19:44:50 +0800

    新增对从右向左的布局的支持

--- lib/src/main/java/dora/widget/DoraTitleBar.kt
+++ lib/src/main/java/dora/widget/DoraTitleBar.kt
@@ -213,7 +213,8 @@ open class DoraTitleBar @JvmOverloads constructor(context: Context, attrs: Attri
      * 支持阿拉伯地区从右向左的布局。
      */
     private fun isRtl() : Boolean {
-        return TextUtilsCompat.getLayoutDirectionFromLocale(Locale.getDefault()) == ViewCompat.LAYOUT_DIRECTION_RTL;
+        return TextUtilsCompat.getLayoutDirectionFromLocale(context.resources.configuration.locale) ==
+                ViewCompat.LAYOUT_DIRECTION_RTL;
     }
 
     /**
@@ -268,9 +269,6 @@ open class DoraTitleBar @JvmOverloads constructor(context: Context, attrs: Attri
      */
     private fun wrapButton(isBackButton: Boolean, iconView: AppCompatImageView) : FrameLayout {
         val box = FrameLayout(context)
-        if (isRtl()) {
-            iconView.rotation = 180f
-        }
         if (isBackButton) {
             iconView.id = R.id.dview_titlebar_back
             iconView.imageTintList = ColorStateList.valueOf(backIconTint)
