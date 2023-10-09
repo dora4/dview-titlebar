a0fb05f45e7252401cc678eb8ff3a776aee2a0aa
dora4 <71242257+dora4@users.noreply.github.com>
2023-10-09 19:44:50 +0800

    新增对从右向左的布局的支持

--- lib/src/main/java/dora/widget/DoraTitleBar.kt
+++ lib/src/main/java/dora/widget/DoraTitleBar.kt
@@ -213,16 +213,17 @@ open class DoraTitleBar @JvmOverloads constructor(context: Context, attrs: Attri
      * 支持阿拉伯地区从右向左的布局。
      */
     private fun isRtl() : Boolean {
-        return TextUtilsCompat.getLayoutDirectionFromLocale(Locale.getDefault()) == ViewCompat.LAYOUT_DIRECTION_RTL;
+        return TextUtilsCompat.getLayoutDirectionFromLocale(context.resources.configuration.locale) ==
+                ViewCompat.LAYOUT_DIRECTION_RTL;
     }
 
     /**
      * 创建默认的返回按钮。
      */
     private fun createBackIcon(context: Context): Drawable {
-        val backIcon = ContextCompat.getDrawable(context, R.drawable.ic_dview_titlebar_back)
-        if (isRtl() && backIcon != null) {
-            DrawableCompat.setLayoutDirection(backIcon, LAYOUT_DIRECTION_RTL)
+        var backIcon = ContextCompat.getDrawable(context, R.drawable.ic_dview_titlebar_back)
+        if (isRtl()) {
+            backIcon = ContextCompat.getDrawable(context, R.drawable.ic_dview_titlebar_close)
         }
         return  backIcon?: BitmapDrawable()
     }
@@ -268,9 +269,6 @@ open class DoraTitleBar @JvmOverloads constructor(context: Context, attrs: Attri
      */
     private fun wrapButton(isBackButton: Boolean, iconView: AppCompatImageView) : FrameLayout {
         val box = FrameLayout(context)
-        if (isRtl()) {
-            iconView.rotation = 180f
-        }
         if (isBackButton) {
             iconView.id = R.id.dview_titlebar_back
             iconView.imageTintList = ColorStateList.valueOf(backIconTint)
